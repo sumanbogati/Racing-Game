8530cf70de73eb55a92ed241570143fef9eaf878
suman <sumanbogati@gmail.com>
2012-09-07 08:37:36 +0530

    made load archietecture and modified on script.js
    made load archietecture and modified on script.js

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
 (
 	function (window, document){
 		window.onload = function (){
-			console.log("are you there");
+			
+			var carRace = function (){
+				this.player = '';
+				this.background = '';
+				this.road = '';
+				this.car = '';
+				this.timing = '';
+			}
+			
+			carRace.prototype =  {
+				init : function (){
+					console.log('very start init');
+				},
+				load : function (){
+					console.log('very start load');
+				}
+			}
+			carRace.init();
+			carRace.load();
+			
+		
+			//class Car
+			var Car  = function (){
+				return {
+					init: function (){
+						console.log('car Init');		
+					}, 
+					
+					load: function (){
+						console.log('car load');		
+					},
+				}
+			}
+			
+			//class Background
+			var Background  = function (){
+				return {
+					init: function (){
+						console.log('Background Init');		
+					}, 
+					
+					load: function (){
+						console.log('Background load');		
+					},
+				}
+			}
+			
+			//class Road
+			var Road  = function (){
+				return {
+					init: function (){
+						console.log('Road Init');		
+					}, 
+					
+					load: function (){
+						console.log('road load');		
+					},
+				}
+			}
 		}
 	}
 )(window, document);
