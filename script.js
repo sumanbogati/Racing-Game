ef0211989150f84d0558b51eb8c826e2c027248f
suman <sumanbogati@gmail.com>
2012-10-03 07:42:16 +0530

    loaded the car
    loaded the car

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+/***
+	this is simple racing game 
+	is created by Suman Bogati, the game inspired from
+	
+	copyright 2012-2012;
+	any enquary please contact at sumanbogati@gmail.com 
+**/
+
 (function (window, document){
 	window.onload = function (){
 		
 		var trackLength   = null; 
-		
 		var width         = 1024;                    // logical canvas width
 		var height        = 768; 
-		var imgLoaded = false;
-		
 		
 		var carRace = function (canvasId){
 			this.cid = canvasId;
@@ -16,6 +21,7 @@
 			this.road = '';
 			this.car = '';
 			this.timing = '';
+			this.sprites = null;
 			return this;
 		}
 		
@@ -31,22 +37,40 @@
 				// background init
 				// this bg should be store into 
 				
+				// this could be into other form or into other way
+				var tImgElm = new Image();
+					tImgElm.src = 'sprites/sprites.png';
+					
+				this.sprites = tImgElm;
+				
 				this.bg = new Background();
-				var bgObj = {type : 'simple', name: 'bg', value : 'sprites/background2.jpg'};
+				var bgObj = {type : 'simple', name: 'bg', value : 'sprites/background5.jpg'};
 				this.bg.init(bgObj);
 				
 				this.rd = new Road(this);
 				this.rd.init();
 		//		this.rd.resetRoad();
+				
+				//car init
+				this.car = new Car(this);
+				this.car.init();
+				
 				this.load();
 				
 				return this;
 			},
 			
 			load : function (){
-				this.bg.load(this, function (mthis){
-					alert("hey guys what is up");
-					mthis.rd.load(mthis.ctx);
+			
+				// this need to be done becuase 
+				// there should be loaded first background images
+				// and should be done other background images.
+				this.bg.load(this, function (cthis){
+					//alert("hey guys what is up");
+					
+					
+					cthis.rd.load(cthis.ctx);
+					cthis.car.load(cthis.ctx);
 				});
 			}
 		};
@@ -77,12 +101,10 @@
 					var imgElem = new Image();
 					
 					imgElem.onload = function (){
-						alert(ctx);
-						ctx.drawImage(imgElem, 0, 0, 300, 200);
+						ctx.drawImage(imgElem, 0, 0, 1050, 500);
 						imgaLoaded(cthis);
 					}
 					imgElem.src = this.value;
-					
 				},
 			}
 		 }
@@ -110,7 +132,7 @@
 								  SKY:  '#72D7EE',
 								  TREE: '#005108',
 								  FOG:  '#005108',
-								  LIGHT:  { road: '#6B6B6B', grass: '#10AA10', rumble: '#555555', lane: '#CCCCCC'  },
+								  LIGHT:  { road: '#6B6B6B', grass: '#5E7542', rumble: '#555555', lane: '#CCCCCC'  },
 								  DARK:   { road: '#696969', grass: '#009A00', rumble: '#BBBBBB'                   },
 								  START:  { road: 'white',   grass: 'white',   rumble: 'white'                     },
 								  FINISH: { road: 'black',   grass: 'black',   rumble: 'black'                     }
@@ -132,7 +154,7 @@
 					var baseSegment = this.findSegment(this.position);
 					var maxy        = height;
 
-					ctx.clearRect(0, 0, width, height);
+				//	ctx.clearRect(0, 0, width, height);
 
 					var n, segment;
 
@@ -141,15 +163,15 @@
 						segment.looped = segment.index < baseSegment.index;
 						//segment.fog    = Util.exponentialFog(n/drawDistance, fogDensity);
 
-						project(segment.p1, (this.playerX * this.roadWidth), this.cameraHeight, this.position - (segment.looped ? this.trackLength : 0), this.cameraDepth, width, height, this.roadWidth);
+						project(segment.p1, (this.playerX * this.roadWidth), this.cameraHeight, this.position - (segment.looped ? this.trackLength : 0), this.cameraDepth, cthis.canvas.width, cthis.canvas.height, this.roadWidth);
 						
-						project(segment.p2, (this.playerX * this.roadWidth), this.cameraHeight, this.position - (segment.looped ? this.trackLength : 0), this.cameraDepth, width, height, this.roadWidth);
+						project(segment.p2, (this.playerX * this.roadWidth), this.cameraHeight, this.position - (segment.looped ? this.trackLength : 0), this.cameraDepth, cthis.canvas.width, cthis.canvas.height, this.roadWidth);
 
 						if ((segment.p1.camera.z <= this.cameraDepth) || // behind us
 						(segment.p2.screen.y >= maxy))          // clip by (already rendered) segment
 						continue;
 
-						mysegment(ctx, width, this.lanes,
+						mysegment(ctx, cthis.canvas.width, this.lanes,
 						segment.p1.screen.x,
 						segment.p1.screen.y,
 						segment.p1.screen.w,
@@ -251,7 +273,8 @@
 
 	//---------------------------------------------------------------------------
 	
-	function rumbleWidth     (projectedRoadWidth, lanes) { return projectedRoadWidth/Math.max(6,  2*lanes); 
+	function rumbleWidth(projectedRoadWidth, lanes) { return projectedRoadWidth/Math.max(6,  2*lanes); 
+	
 	}
   
 	 function laneMarkerWidth(projectedRoadWidth, lanes) { return projectedRoadWidth/Math.max(32, 8*lanes); }
@@ -265,11 +288,11 @@
 
 		//ctx.fillStyle = color.grass;
 		
-		/*ctx.fillStyle = color.grass;
-		ctx.fillRect(0, y2, width, y1 - y2);*/
+		ctx.fillStyle = color.grass;
+		ctx.fillRect(0, y2, width, y1 - y2);
 		
-		ctx.fillStyle = "red";
-		ctx.fillRect(0, 106, 1024, 80);
+		/*ctx.fillStyle = "red";
+		ctx.fillRect(0, 106, 1024, 80);*/
 		
 
 		polygon(ctx, x1-w1-r1, y1, x1-w1, y1, x2-w2, y2, x2-w2-r2, y2, color.rumble);
@@ -289,17 +312,62 @@
 	}
 		 
 		//class Car
-		var Car  = function (){
+		var Car  = function (cthis){
 			return {
-				init: function (obj){
-					this.name = obj.name;
-					this.type = obj.type;						
+				init: function (){
+				//	this.name = obj.name;
+				//	this.type = obj.type;
+					this.destX = width/2;
+					this.destY = height;
+					this.resolution = 1.6;
+					this.scale = cthis.rd.cameraDepth/cthis.rd.playerZ;
+					this.maxSpeed  = cthis.rd.segmentLength/cthis.step;
+					this.speed = 0;
+					this.speedPercent = this.maxSpeed/this.speed;
+					this.keyLeft = false;
+					this.keyRight = false;
+					this.steer =  this.speed * (this.keyLeft ? -1 : this.keyRight ? 1 : 0);
+					this.updown = 0;
 				}, 
 				
 				//here would be the code for run, stop, start
-				load: function (obj){
-					//this.run();
+				//here is the load instasd player
+				load: function (){
+				
+						var bounce = (1.5 * Math.random() * this.speedPercent * this.resolution) * randomChoice([-1,1]);
+						var sprite;
+						if (this.steer < 0)
+						  sprite = (this.updown > 0) ? SPRITES.PLAYER_UPHILL_LEFT : SPRITES.PLAYER_LEFT;
+						else if (this.steer > 0)
+						  sprite = (this.updown > 0) ? SPRITES.PLAYER_UPHILL_RIGHT : SPRITES.PLAYER_RIGHT;
+						else
+						  sprite = (this.updown > 0) ? SPRITES.PLAYER_UPHILL_STRAIGHT : SPRITES.PLAYER_STRAIGHT;
+						
+						this.destY = this.destY + bounce;
+						this.sprite(sprite, -0.5, -1);
 				},
+				
+				sprite: function(sprite, offsetX, offsetY, clipY) {
+					//  scale for projection AND relative to roadWidth (for tweakUI)
+					var destW  = (sprite.w * this.scale * width/2) * (SPRITES.SCALE * cthis.rd.roadWidth);
+					var destH  = (sprite.h * this.scale * width/2) * (SPRITES.SCALE * cthis.rd.roadWidth);
+
+					this.destX = this.destX + (destW * (offsetX || 0));
+					this.destY = this.destY + (destH * (offsetY || 0));
+
+					var clipH = clipY ? Math.max(0, this.destY+destH-clipY) : 0;
+					if (clipH < destH)
+						
+						//TODO this should be dynamic
+					  this.destY = 610.56;
+						
+					 // alert(cthis.sprites);
+				cthis.ctx.drawImage(cthis.sprites, sprite.x, sprite.y, sprite.w, sprite.h - (sprite.h*clipH/destH), this.destX, this.destY, destW, destH - clipH);
+					
+					
+					
+					
+				}, 
 				
 				run : function (){
 					console.log('car run');
@@ -331,8 +399,64 @@
 			}	
 			
 		}
-		
-		var myCar = new carRace("racingCan");
+	
+//TODO
+//this need to be done into CLASS	
+  var SPRITES = {
+  PALM_TREE:              { x:    5, y:    5, w:  215, h:  540 },
+  BILLBOARD08:            { x:  230, y:    5, w:  385, h:  265 },
+  TREE1:                  { x:  625, y:    5, w:  360, h:  360 },
+  DEAD_TREE1:             { x:    5, y:  555, w:  135, h:  332 },
+  BILLBOARD09:            { x:  150, y:  555, w:  328, h:  282 },
+  BOULDER3:               { x:  230, y:  280, w:  320, h:  220 },
+  COLUMN:                 { x:  995, y:    5, w:  200, h:  315 },
+  BILLBOARD01:            { x:  625, y:  375, w:  300, h:  170 },
+  BILLBOARD06:            { x:  488, y:  555, w:  298, h:  190 },
+  BILLBOARD05:            { x:    5, y:  897, w:  298, h:  190 },
+  BILLBOARD07:            { x:  313, y:  897, w:  298, h:  190 },
+  BOULDER2:               { x:  621, y:  897, w:  298, h:  140 },
+  TREE2:                  { x: 1205, y:    5, w:  282, h:  295 },
+  BILLBOARD04:            { x: 1205, y:  310, w:  268, h:  170 },
+  DEAD_TREE2:             { x: 1205, y:  490, w:  150, h:  260 },
+  BOULDER1:               { x: 1205, y:  760, w:  168, h:  248 },
+  BUSH1:                  { x:    5, y: 1097, w:  240, h:  155 },
+  CACTUS:                 { x:  929, y:  897, w:  235, h:  118 },
+  BUSH2:                  { x:  255, y: 1097, w:  232, h:  152 },
+  BILLBOARD03:            { x:    5, y: 1262, w:  230, h:  220 },
+  BILLBOARD02:            { x:  245, y: 1262, w:  215, h:  220 },
+  STUMP:                  { x:  995, y:  330, w:  195, h:  140 },
+  SEMI:                   { x: 1365, y:  490, w:  122, h:  144 },
+  TRUCK:                  { x: 1365, y:  644, w:  100, h:   78 },
+  CAR03:                  { x: 1383, y:  760, w:   88, h:   55 },
+  CAR02:                  { x: 1383, y:  825, w:   80, h:   59 },
+  CAR04:                  { x: 1383, y:  894, w:   80, h:   57 },
+  CAR01:                  { x: 1205, y: 1018, w:   80, h:   56 },
+  PLAYER_UPHILL_LEFT:     { x: 1383, y:  961, w:   80, h:   45 },
+  PLAYER_UPHILL_STRAIGHT: { x: 1295, y: 1018, w:   80, h:   45 },
+  PLAYER_UPHILL_RIGHT:    { x: 1385, y: 1018, w:   80, h:   45 },
+  PLAYER_LEFT:            { x:  995, y:  480, w:   80, h:   41 },
+  PLAYER_STRAIGHT:        { x: 1085, y:  480, w:   80, h:   41 },
+  PLAYER_RIGHT:           { x:  995, y:  531, w:   80, h:   41 }
+};
+
+SPRITES.SCALE = 0.3 * (1/SPRITES.PLAYER_STRAIGHT.w) // the reference sprite width should be 1/3rd the (half-)roadWidth
+
+SPRITES.BILLBOARDS = [SPRITES.BILLBOARD01, SPRITES.BILLBOARD02, SPRITES.BILLBOARD03, SPRITES.BILLBOARD04, SPRITES.BILLBOARD05, SPRITES.BILLBOARD06, SPRITES.BILLBOARD07, SPRITES.BILLBOARD08, SPRITES.BILLBOARD09];
+SPRITES.PLANTS     = [SPRITES.TREE1, SPRITES.TREE2, SPRITES.DEAD_TREE1, SPRITES.DEAD_TREE2, SPRITES.PALM_TREE, SPRITES.BUSH1, SPRITES.BUSH2, SPRITES.CACTUS, SPRITES.STUMP, SPRITES.BOULDER1, SPRITES.BOULDER2, SPRITES.BOULDER3];
+SPRITES.CARS       = [SPRITES.CAR01, SPRITES.CAR02, SPRITES.CAR03, SPRITES.CAR04, SPRITES.SEMI, SPRITES.TRUCK];
+
+function randomChoice(options) { 
+	return options[randomInt(0, options.length-1)];  
+}
+
+
+
+ function randomInt(min, max)          { return Math.round(interpolate(min, max, Math.random()));   }
+
+    function   interpolate(a,b,percent)       { return a + (b-a)*percent}
+  
+  
+var myCar = new carRace("racingCan");
 		myCar.init(myCar.cid);
 	}
 })(window, document);
