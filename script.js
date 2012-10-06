4e41e597d906f8ef01106f70110d1bb239e90449
suman <sumanbogati@gmail.com>
2012-10-06 09:28:43 +0530

    organized the script
    organized the script

--- script.js
+++ script.js
@@ -62,6 +62,18 @@
 				
 				this.load();
 				
+				
+				//this is to be splitted in relative calss
+				this.SPRITES = {
+				  CAR01:                  { x: 1205, y: 1018, w:   80, h:   56 },
+				  PLAYER_STRAIGHT:        { x: 1085, y:  480, w:   80, h:   41 },
+				};
+				
+				
+				this.SPRITES.SCALE = 0.3 * (1/this.SPRITES.PLAYER_STRAIGHT.w); // the reference sprite width should be 1/3rd the (half-)roadWidth
+				
+				//this is to be splitted in relative calss
+				this.SPRITES.CARS = [this.SPRITES.CAR01];
 				return this;
 			},
 			
@@ -328,19 +340,16 @@
 				//here would be the code for run, stop, start
 				//here is the load instasd player
 				load: function (){
-						console.log('speedPercent ' + this.speedPercent);
-						console.log('this.resolution ' + this.resolution);
-						console.log('randomChoice ' + randomChoice([-1,1]));
 						
-						var bounce = (1.5 * Math.random() * this.speedPercent * this.resolution) * randomChoice([-1,1]);
+						var bounce = (1.5 * Math.random() * this.speedPercent * this.resolution) * this.mechanism().randomChoice([-1,1]);
 						
 						var sprite;
 						if (this.steer < 0)
-						  sprite = (this.updown > 0) ? SPRITES.PLAYER_UPHILL_LEFT : SPRITES.PLAYER_LEFT;
+						  sprite = (this.updown > 0) ? cthis.SPRITES.PLAYER_UPHILL_LEFT : cthis.SPRITES.PLAYER_LEFT;
 						else if (this.steer > 0)
-						  sprite = (this.updown > 0) ? SPRITES.PLAYER_UPHILL_RIGHT : SPRITES.PLAYER_RIGHT;
+						  sprite = (this.updown > 0) ? cthis.SPRITES.PLAYER_UPHILL_RIGHT : cthis.SPRITES.PLAYER_RIGHT;
 						else
-						  sprite = (this.updown > 0) ? SPRITES.PLAYER_UPHILL_STRAIGHT : SPRITES.PLAYER_STRAIGHT;
+						  sprite = (this.updown > 0) ? cthis.SPRITES.PLAYER_UPHILL_STRAIGHT : cthis.SPRITES.PLAYER_STRAIGHT;
 						//console.log('bounce ' + bounce);
 						this.destY = this.destY + bounce;
 						this.sprite(sprite, -0.5, -1);
@@ -348,8 +357,8 @@
 				
 				sprite: function(sprite, offsetX, offsetY, clipY) {
 					//  scale for projection AND relative to roadWidth (for tweakUI)
-					var destW  = (sprite.w * this.scale * width/2) * (SPRITES.SCALE * cthis.rd.roadWidth);
-					var destH  = (sprite.h * this.scale * width/2) * (SPRITES.SCALE * cthis.rd.roadWidth);
+					var destW  = (sprite.w * this.scale * width/2) * (cthis.SPRITES.SCALE * cthis.rd.roadWidth);
+					var destH  = (sprite.h * this.scale * width/2) * (cthis.SPRITES.SCALE * cthis.rd.roadWidth);
 
 					this.destX = this.destX + (destW * (offsetX || 0));
 					this.destY = this.destY + (destH * (offsetY || 0));
@@ -369,6 +378,22 @@
 				
 				run : function (){
 					//console.log('car run');
+				},
+				
+				mechanism : function (){
+					return {
+						randomChoice : function (options){
+							return options[this.randomInt(0, options.length-1)]; 
+						},
+						
+						randomInt : function (min, max) {
+							return Math.round(this.interpolate(min, max, Math.random()));   
+						},
+						
+						interpolate : function  (a,b,percent) {
+							return a + (b-a)*percent;
+						}
+					}
 				}
 			}
 		}
@@ -397,64 +422,7 @@
 			}	
 		}
 	
-//TODO
-//this need to be done into CLASS	
-  var SPRITES = {
-  PALM_TREE:              { x:    5, y:    5, w:  215, h:  540 },
-  BILLBOARD08:            { x:  230, y:    5, w:  385, h:  265 },
-  TREE1:                  { x:  625, y:    5, w:  360, h:  360 },
-  DEAD_TREE1:             { x:    5, y:  555, w:  135, h:  332 },
-  BILLBOARD09:            { x:  150, y:  555, w:  328, h:  282 },
-  BOULDER3:               { x:  230, y:  280, w:  320, h:  220 },
-  COLUMN:                 { x:  995, y:    5, w:  200, h:  315 },
-  BILLBOARD01:            { x:  625, y:  375, w:  300, h:  170 },
-  BILLBOARD06:            { x:  488, y:  555, w:  298, h:  190 },
-  BILLBOARD05:            { x:    5, y:  897, w:  298, h:  190 },
-  BILLBOARD07:            { x:  313, y:  897, w:  298, h:  190 },
-  BOULDER2:               { x:  621, y:  897, w:  298, h:  140 },
-  TREE2:                  { x: 1205, y:    5, w:  282, h:  295 },
-  BILLBOARD04:            { x: 1205, y:  310, w:  268, h:  170 },
-  DEAD_TREE2:             { x: 1205, y:  490, w:  150, h:  260 },
-  BOULDER1:               { x: 1205, y:  760, w:  168, h:  248 },
-  BUSH1:                  { x:    5, y: 1097, w:  240, h:  155 },
-  CACTUS:                 { x:  929, y:  897, w:  235, h:  118 },
-  BUSH2:                  { x:  255, y: 1097, w:  232, h:  152 },
-  BILLBOARD03:            { x:    5, y: 1262, w:  230, h:  220 },
-  BILLBOARD02:            { x:  245, y: 1262, w:  215, h:  220 },
-  STUMP:                  { x:  995, y:  330, w:  195, h:  140 },
-  SEMI:                   { x: 1365, y:  490, w:  122, h:  144 },
-  TRUCK:                  { x: 1365, y:  644, w:  100, h:   78 },
-  CAR03:                  { x: 1383, y:  760, w:   88, h:   55 },
-  CAR02:                  { x: 1383, y:  825, w:   80, h:   59 },
-  CAR04:                  { x: 1383, y:  894, w:   80, h:   57 },
-  CAR01:                  { x: 1205, y: 1018, w:   80, h:   56 },
-  PLAYER_UPHILL_LEFT:     { x: 1383, y:  961, w:   80, h:   45 },
-  PLAYER_UPHILL_STRAIGHT: { x: 1295, y: 1018, w:   80, h:   45 },
-  PLAYER_UPHILL_RIGHT:    { x: 1385, y: 1018, w:   80, h:   45 },
-  PLAYER_LEFT:            { x:  995, y:  480, w:   80, h:   41 },
-  PLAYER_STRAIGHT:        { x: 1085, y:  480, w:   80, h:   41 },
-  PLAYER_RIGHT:           { x:  995, y:  531, w:   80, h:   41 }
-};
-
-SPRITES.SCALE = 0.3 * (1/SPRITES.PLAYER_STRAIGHT.w) // the reference sprite width should be 1/3rd the (half-)roadWidth
-
-SPRITES.BILLBOARDS = [SPRITES.BILLBOARD01, SPRITES.BILLBOARD02, SPRITES.BILLBOARD03, SPRITES.BILLBOARD04, SPRITES.BILLBOARD05, SPRITES.BILLBOARD06, SPRITES.BILLBOARD07, SPRITES.BILLBOARD08, SPRITES.BILLBOARD09];
-SPRITES.PLANTS     = [SPRITES.TREE1, SPRITES.TREE2, SPRITES.DEAD_TREE1, SPRITES.DEAD_TREE2, SPRITES.PALM_TREE, SPRITES.BUSH1, SPRITES.BUSH2, SPRITES.CACTUS, SPRITES.STUMP, SPRITES.BOULDER1, SPRITES.BOULDER2, SPRITES.BOULDER3];
-SPRITES.CARS       = [SPRITES.CAR01, SPRITES.CAR02, SPRITES.CAR03, SPRITES.CAR04, SPRITES.SEMI, SPRITES.TRUCK];
-
-function randomChoice(options) { 
-	return options[randomInt(0, options.length-1)];  
-}
-
-function randomInt(min, max) { 
-	return Math.round(interpolate(min, max, Math.random()));   
-}
-
-function   interpolate(a,b,percent) { 
-	return a + (b-a)*percent;
-}
-  
-  
+	
 var myCar = new carRace("racingCan");
 		myCar.init(myCar.cid);
 	}
